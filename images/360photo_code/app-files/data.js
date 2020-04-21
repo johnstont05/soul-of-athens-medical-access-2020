cc5153c82d97e40ed41979218aac7ae6d575a905
Taylor Johnston <tj369915@ohio.edu>
2020-04-20 21:57:23 -0400

    vimeo

--- images/360photo_code/app-files/data.js
+++ images/360photo_code/app-files/data.js
@@ -30,8 +30,8 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.37390997871160003,
-          "pitch": 0.18903155657215365,
+          "yaw": -0.35781439228650136,
+          "pitch": 0.11449170786818996,
           "rotation": 0,
           "target": "2-exam-room-2"
         },
@@ -44,46 +44,46 @@ var APP_DATA = {
       ],
       "infoHotspots": [
         {
-          "yaw": -2.464913479719339,
-          "pitch": 0.5238584843440712,
+          "yaw": -2.464592880239806,
+          "pitch": 0.520694637361661,
           "title": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;\">Check-in Desk&nbsp;</span>",
-          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">This is where patients check in to be seen&nbsp;</span>"
+          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">This is where patients check in with the nurse and are given clipboards to fill our their information. Walk-ins are welcome and appointments are encouraged.&nbsp;</span>"
         },
         {
           "yaw": -3.0532050859545237,
           "pitch": 0.18112199486297875,
           "title": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;\">Refrigerator for Antibiotics</span>",
-          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">Some antibiotics must be kept at a certain temperature</span>"
+          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">Antibiotics are only stocked while the truck is operating and no narcotics or patient medications are allowed on the mobile health unit.&nbsp;</span>"
         },
         {
           "yaw": -1.579560790940274,
           "pitch": 0.6415225453280016,
           "title": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;\">Patient Waiting Chair</span>",
-          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">This is where patients wait to see the doctor</span><br>"
+          "text": "<font face=\"Helvetica Neue, Helvetica, Arial, sans-serif\"><span style=\"font-size: 16px;\">This is where patients wait to receive&nbsp;care while other patients are being treated.&nbsp;</span></font><br>"
         },
         {
           "yaw": -2.2031720276378692,
           "pitch": -1.4085578954175215,
-          "title": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;\">Skylight</span>",
-          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">Used to calm patients</span>"
+          "title": "<font face=\"Helvetica Neue, Helvetica, Arial, sans-serif\">Calming Atmosphere</font>",
+          "text": "<font face=\"Helvetica Neue, Helvetica, Arial, sans-serif\"><span style=\"font-size: 16px;\">Lit photographs are used to calm patients during their visit.&nbsp;</span></font>"
         },
         {
           "yaw": -0.9271607958605195,
           "pitch": 0.4716186351521898,
           "title": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;\">Handicap Accessible Bathroom</span>",
-          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">The waiting chair folds up to allow access to this bathroom</span>"
+          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">The waiting chair folds up to allow access to this bathroom for patients that have accessibility needs.</span>"
         },
         {
           "yaw": 0.41891594744852867,
           "pitch": 0.6172317643269167,
           "title": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;\">Chair Holder</span>",
-          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">Holds chair while the truck is in motion</span>"
+          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">This holds the nurse's chair when the clinic is closed and the truck is in motion.</span>"
         },
         {
           "yaw": -0.10718057075518672,
           "pitch": 0.21308598581019567,
           "title": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;\">Entrance</span>",
-          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">Front door to the truck</span>"
+          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">Steps lead patients up to the front door.</span>"
         }
       ]
     },
@@ -128,19 +128,25 @@ var APP_DATA = {
           "yaw": 0.6158121445706932,
           "pitch": 0.16097704800192325,
           "title": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;\">Handicap Accessible Entrance</span>",
-          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">Patients are lifted to the height of the truck and directly into the exam room</span>"
+          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">Patients are lifted to the height of the truck and directly into the exam room. Some patients may have knee or hip issues that make climbing the front steps to the truck difficult.&nbsp;</span>"
         },
         {
           "yaw": -2.094345320067003,
           "pitch": 0.07928008595688851,
-          "title": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;\">Doctor Tools</span>",
-          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">Blood Pressure Monitor, Thermometer, Otoscope, Ophthalmoscope and Thermometer</span>"
+          "title": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;\">Examination Equipment</span>",
+          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">Patient exam rooms are equipped like any standard exam room including a blood pressure monitor, thermometer, otoscope and ophthalmoscope.&nbsp;</span><br>"
         },
         {
           "yaw": -1.35058126296153,
           "pitch": 0.11264148438879218,
           "title": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;\">Privacy Curtain</span>",
-          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">Privacy curtain that covers the exam chair</span>"
+          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">Privacy curtain that covers the exam chair. The door can also be shut for patient privacy.&nbsp;</span>"
+        },
+        {
+          "yaw": -1.629446082527359,
+          "pitch": -1.118422788650431,
+          "title": "Calming Atmosphere",
+          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">Lit photographs are used to calm patients during their visit.</span>"
         }
       ]
     },
@@ -174,8 +180,8 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 2.1541215294639358,
-          "pitch": 0.12512156563740007,
+          "yaw": 2.156429725942088,
+          "pitch": 0.10173168203841954,
           "rotation": 0,
           "target": "0-lobby"
         }
@@ -185,25 +191,31 @@ var APP_DATA = {
           "yaw": 2.1074074135850935,
           "pitch": 0.40953383014070255,
           "title": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;\">Door to Lobby</span>",
-          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">Door can be shut for privacy while seeing patients</span>"
+          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">Door can be shut for patient privacy.&nbsp;</span>"
         },
         {
-          "yaw": -1.7416481959433359,
-          "pitch": -1.4387760409740444,
-          "title": "Skylight&nbsp;",
-          "text": "Used to calm patients"
+          "yaw": -2.226506768272941,
+          "pitch": -1.307929221986404,
+          "title": "Calming Atmosphere",
+          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">Lit photographs are used to calm patients during their visit.</span>"
         },
         {
           "yaw": -1.953418138678554,
           "pitch": 0.013674131249729271,
-          "title": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;\">Doctor Tools</span>",
-          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">Blood Pressure Monitor, Thermometer, Otoscope, Ophthalmoscope and Thermometer</span>"
+          "title": "Examination Equipment",
+          "text": "<span style=\"font-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif; font-size: 16px;\">Patient exam rooms are equipped like any standard exam room including a blood pressure monitor, thermometer, otoscope and ophthalmoscope.&nbsp;</span><br>"
         },
         {
-          "yaw": -1.4172280919553302,
-          "pitch": -0.17293806898865682,
+          "yaw": -1.3796733046601268,
+          "pitch": -0.15950978608938726,
           "title": "Microwave",
-          "text": "For staff use during long shifts"
+          "text": "For staff use during long shifts."
+        },
+        {
+          "yaw": 2.9109808829709607,
+          "pitch": 0.10689813616590982,
+          "title": "Medical Door&nbsp;",
+          "text": "Connects the bathroom to the exam room for the nurse to retrieve urine samples."
         }
       ]
     }
